7b6887f43a40e82bde46f4c4462161bf9913197a
Abdulmumin Abdulkarim <avdorr12345@gmail.com>
2022-08-20 11:57:56 +0100

    code docs

--- xologic/main.py
+++ xologic/main.py
@@ -2,14 +2,14 @@ class Logic():
     """
     Logic for tic-tac-toe board
     """
+
     def __init__(self, board_size=4):
         self.board_size = board_size
         self.win_size = 2
         self.default = ['' for i in range(self.board_size**2)]
         self.board = self.default[:]
-        
 
-    def logic(self, board=None) -> list:
+    def check(self, board=None, board_size=None) -> list:
         """
         returns the winner 
         and a list of the indexes where the win was found!
@@ -18,6 +18,8 @@ class Logic():
         """
         if not board:
             board = self.board
+        else:
+            if not board_size: return "board_size required for a custom board"
         horizontal = self.horizontal(board)
         # print(<'horizontal checked')
         vertical = self.vertical(board)
@@ -42,7 +44,7 @@ class Logic():
         number_indexes = [i for i in range(self.board_size)]
         # linux = map(lambda x:board[at+x], line)
         for i in range(self.board_size):
-            pos = list(map(lambda x:board[at+x], number_indexes))
+            pos = list(map(lambda x: board[at+x], number_indexes))
             # print(pos)
             if self.hole(pos, board[at]):
                 return board[at], [j+at for j in number_indexes]
@@ -52,30 +54,33 @@ class Logic():
     def vertical(self, board):
         at = 0
         for i in range(self.board_size):
-            number_indexes = [i+at for i in range(0, self.board_size**2, self.board_size)]
+            number_indexes = [
+                i+at for i in range(0, self.board_size**2, self.board_size)]
             line = map(lambda x: board[x], number_indexes)
             if self.hole(line, board[at]):
-                return  board[at], number_indexes
+                return board[at], number_indexes
             at += 1
 
         return False
 
     def diagonal_1(self, board):
-        
-        number_indexes = [i for i in range(0, self.board_size**2, self.board_size+1)]
-        pos = map(lambda x:board[x], number_indexes)
+
+        number_indexes = [i for i in range(
+            0, self.board_size**2, self.board_size+1)]
+        pos = map(lambda x: board[x], number_indexes)
         # print(number_indexes)
         if self.hole(pos, board[0]):
             return board[0], number_indexes
         return False
 
     def diagonal_2(self, board):
-        number_indexes = [i for i in range(self.board_size-1, self.board_size**2, self.board_size-1)][:self.board_size]
+        number_indexes = [i for i in range(
+            self.board_size-1, self.board_size**2, self.board_size-1)][:self.board_size]
         # print(number_indexes)
-        pos = map(lambda x:board[x], number_indexes)
+        pos = map(lambda x: board[x], number_indexes)
         if self.hole(pos, board[self.board_size-1]):
-            return  board[self.board_size-1], number_indexes
-        
+            return board[self.board_size-1], number_indexes
+
         return False
 
     def hole(self, list_, val):
@@ -89,6 +94,7 @@ class Logic():
                 return all_values
 
         return all_values
+
     def black_hole(self, list_, val):
         count = 0
         for i in list_:
@@ -98,4 +104,3 @@ class Logic():
                 return False
             if count == self.win_size:
                 return True
-
