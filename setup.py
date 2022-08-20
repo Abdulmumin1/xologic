7ffed8550a7030df1ec7df4d069e0aa5f0273edf
Abdulmumin Abdulkarim <avdorr12345@gmail.com>
2022-08-20 12:08:42 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='xologic',
-    version='0.1.0',    
+    version='0.1.0',
     description='a tic tac toe logic to get winning team',
     url='https://github.com/Abdulmumin1/xologic',
     author='Abdulmumin Abdulkarim',
@@ -12,10 +12,10 @@ setup(
     install_requires=[],
 
     classifiers=[
-        'Development Status :: Completed',
+        'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',  
-        'Operating System :: POSIX :: Linux',        
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
